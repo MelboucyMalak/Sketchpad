7f7333396e96ef22883ee68aaf0c01ffe277be61
Sooqyip <192369409+MelboucyMalak@users.noreply.github.com>
2025-09-04 16:27:55 +0100

    Fix overflow and responsive problem by adding aspect-ratio: 1 / 1 to the container
    Added other buttons:
    -Normal mode as a black color instead of the rainbow brush by default
    -Shadowing mode
    -erase and erase all
    
    Fix mode switching by adding a "mode" variable so the that the listener added depends on it

--- js.js
+++ js.js
@@ -1,6 +1,60 @@
-const btn=document.querySelector("button")
-btn.addEventListener("click",()=>{
-    let resolution=''
+let resolution=16
+let mode='draw'
+let pixels=[]
+let color=""
+const div=document.querySelector(".container")
+generateGrid(16)
+
+function generateGrid(resolution){
+    div.innerHTML=""
+    for(let i=1;i<=resolution;i++){
+        const child=document.createElement("div")
+        child.classList.add("child")
+        div.appendChild(child)
+        for(let j=1;j<=resolution;j++){
+            const grandChild=document.createElement("div")
+            child.appendChild(grandChild)
+            grandChild.classList.add("pixel")
+        }
+    }
+
+    pixels=document.querySelectorAll(".pixel")
+    pixels.forEach(pixel => {
+        pixel.addEventListener("mouseover",(e)=>{
+            switch (mode){
+                case "rainbow":
+                    color=`rgb(${rand()},${rand()},${rand()})`
+                    e.target.style.backgroundColor=color
+                    break;
+                case "erase":
+                    e.target.style.backgroundColor="white"
+                    break;
+                case "darken":
+                    color = window.getComputedStyle(e.target).backgroundColor;
+                    if(color==="rgba(0, 0, 0, 0)"){color="rgb(255, 255, 255)"}
+                    color = color.replace("rgb(", "").replace(")", "");
+                    let parts = color.split(",");
+
+                    let r = Number(parts[0].trim());
+                    let g = Number(parts[1].trim());
+                    let b = Number(parts[2].trim());
+
+                    const newR = Math.max(0, Math.floor(r - r * 0.1));
+                    const newG = Math.max(0, Math.floor(g - g * 0.1));
+                    const newB = Math.max(0, Math.floor(b - b * 0.1));
+
+                    e.target.style.backgroundColor=`rgb(${newR}, ${newG}, ${newB})`;
+                    break;
+                case "draw":
+                    e.target.style.backgroundColor="black"
+                    break;
+            }
+        })
+    })
+}
+
+const btnNew=document.querySelector("#new")
+btnNew.addEventListener("click",()=>{
     while(true){
     let input= prompt("How many square per side? (doen't exceed 100)")
     if(input===null){return}
@@ -10,39 +64,30 @@ btn.addEventListener("click",()=>{
     alert(`Generating grid with ${resolution} per line`)
     generateGrid(resolution)
 })
- 
 
-function generateGrid(resolution){
-    div.innerHTML=""
-    for(let i=1;i<=resolution;i++){
-        const child=document.createElement("div")
-        child.classList.add("child")
-        div.appendChild(child)
-        child.style.display="flex"
-        for(let j=1;j<=resolution;j++){
-            const grandChild=document.createElement("div")
-            child.appendChild(grandChild)
-            grandChild.classList.add("pixel")
-        }
-    }
-    const pixels=document.querySelectorAll(".pixel")
-    pixels.forEach(pixel => {
-        pixel.addEventListener("mouseover",(e)=>{
-            let color=e.target.style.backgroundColor=`rgb(${rand()},${rand()},${rand()})`
-            e.target.style.backgroundColor=color
-            e.target.style.borderColor=color
-        })
-    });
-}
+const btnNormal=document.querySelector("#normal")
+btnNormal.addEventListener("click",()=>{mode="draw"})
 
+const btnDark=document.querySelector("#dark")
+btnDark.addEventListener("click",()=>{mode='darken'})
 
+const btnRnbw=document.querySelector("#rainbow")
+btnRnbw.addEventListener("click",()=>{mode='rainbow'})
 
-const div=document.querySelector(".container")
+const btnErase=document.querySelector("#erase")
+btnErase.addEventListener("click",()=>{mode='erase'})
 
-
-generateGrid(16)
+const btnEraseAll=document.querySelector("#eraseAll")
+btnEraseAll.addEventListener("click",()=> generateGrid(resolution))
 
 function rand(){
     return Math.floor(Math.random()*256+1)
 }
+
+ 
+
+ 
+ 
+
+ 
 
