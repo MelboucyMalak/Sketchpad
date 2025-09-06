3e1b8b7090c128943ff899154b44eaa02bf7ae49
Sooqyip <192369409+MelboucyMalak@users.noreply.github.com>
2025-09-06 15:14:54 +0100

    fix drawing on mobile

--- js.js
+++ js.js
@@ -39,42 +39,42 @@ function generateGrid(resolution){
             grandChild.setAttribute('draggable', 'false');
         }
     }
-
-    pixels=document.querySelectorAll(".pixel")
-    pixels.forEach(pixel => {
-        pixel.addEventListener("pointermove",(e)=>{
-            if(!isDrawing){return;}
-            switch (mode){
-                case "rainbow":
-                    color=`rgb(${rand()},${rand()},${rand()})`
-                    e.target.style.backgroundColor=color
-                    break;
-                case "erase":
-                    e.target.style.backgroundColor="white"
-                    break;
-                case "darken":
-                    color = window.getComputedStyle(e.target).backgroundColor;
-                    if(color==="rgba(0, 0, 0, 0)"){color="rgb(255, 255, 255)"}
-                    color = color.replace("rgb(", "").replace(")", "");
-                    let parts = color.split(",");
-
-                    let r = Number(parts[0].trim());
-                    let g = Number(parts[1].trim());
-                    let b = Number(parts[2].trim());
-
-                    const newR = Math.max(0, Math.floor(r - r * 0.1));
-                    const newG = Math.max(0, Math.floor(g - g * 0.1));
-                    const newB = Math.max(0, Math.floor(b - b * 0.1));
-
-                    e.target.style.backgroundColor=`rgb(${newR}, ${newG}, ${newB})`;
-                    break;
-                case "draw":
-                    e.target.style.backgroundColor="black"
-                    break;
-            }
-        })
-    })
 }
+
+document.addEventListener("pointermove", (e)=>{
+    if(!isDrawing) return;
+    if(e.target.classList.contains("pixel")){
+        switch (mode){
+            case "rainbow":
+                color=`rgb(${rand()},${rand()},${rand()})`
+                e.target.style.backgroundColor=color
+                break;
+            case "erase":
+                e.target.style.backgroundColor="white"
+                break;
+            case "darken":
+                color = window.getComputedStyle(e.target).backgroundColor;
+                if(color==="rgba(0, 0, 0, 0)"){color="rgb(255, 255, 255)"}
+                color = color.replace("rgb(", "").replace(")", "");
+                let parts = color.split(",");
+
+                let r = Number(parts[0].trim());
+                let g = Number(parts[1].trim());
+                let b = Number(parts[2].trim());
+
+                const newR = Math.max(0, Math.floor(r - r * 0.1));
+                const newG = Math.max(0, Math.floor(g - g * 0.1));
+                const newB = Math.max(0, Math.floor(b - b * 0.1));
+
+                e.target.style.backgroundColor=`rgb(${newR}, ${newG}, ${newB})`;
+                break;
+            case "draw":
+                e.target.style.backgroundColor="black"
+                break;
+        }
+    }
+})
+
 
 const btnNew=document.querySelector("#new")
 btnNew.addEventListener("click",()=>{
